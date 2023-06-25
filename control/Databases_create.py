54cd92efaec43a7c9b61b3ec2522644fe45f1122
KatoriDGT <87225454+KatoriDGT@users.noreply.github.com>
2023-06-25 21:33:54 +0700

    Delete control directory

--- control/Databases_create.py
+++
@@ -1,9 +0,0 @@
-from Databases import Base, Links_point, Seller
-from connect import engine
-import logging, sqlalchemy
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.DEBUG)
-print("Creating >>>")
-Base.metadata.drop_all(bind=engine)
-Base.metadata.create_all(bind=engine)
-print("Done!")
