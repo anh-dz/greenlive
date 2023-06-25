4fe09c3ea94a25f4b098e307db0cf56874a72671
Noob Cừu <ohtuananhkt@gmail.com>
2023-06-25 21:39:15 +0700

    bi loi

---
+++ control/History_user_db.py
@@ -0,0 +1,7 @@
+from Databases import History_user_mn, User, Seller, Buyer
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from connect import engine
+class History_user_mn():
+    pass
+#test join naooo
