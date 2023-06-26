bf59417591e0255b403424f9bc3b474ce4dbaa7f
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2023-06-26 23:11:05 +0700

    Add files via upload

--- control/__init__.py
+++ control/__init__.py
@@ -1,14 +1,3 @@
-from flask import Flask
-
-# def create_app():
-#     app = Flask(__name__)
-
-#     from .home import homepage
-#     from .reg import register
-#     from .login import login_bp
-
-#     app.register_blueprint(homepage, url_prefix="/")
-#     app.register_blueprint(register, url_prefix="/")
-#     app.register_blueprint(login_bp, url_prefix="/")
-
-#     return app
+from flask import *
+from .a_log_in import *
+from .a_sign_up import *
