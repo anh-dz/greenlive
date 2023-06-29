536ade6466310ca30c5a3c563c9a6bfc181f1863
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2023-06-29 17:36:29 +0700

    Update __init__.py

--- control/__init__.py
+++ control/__init__.py
@@ -1,4 +1,5 @@
 from flask import *
+from flask_login import *
 from .User_db import *
 from .Buyer_db import *
 from .a_log_in import *
@@ -8,12 +9,11 @@ from .History_user_db import *
 from .historyReadData import *
 from .item_buy import *
 from .QR_maker import *
-from .check_cookie import *
+#from .check_cookie import *
 from .QR_db import *
-from .Note_db import *
-from .Item_db import *
-# from .QR_maker import *
-# so_diem = get_id()
-# thong_ke_doi_emerald = tkdoidiem
-# lich_su_tich_emerald = tktichdiem
-# from .Databases_create import *
+#from .Databases_create import *
+from .QR_maker import *
+
+so_diem = get_id()
+thong_ke_doi_emerald = tkdoidiem
+lich_su_tich_emerald = tktichdiem
