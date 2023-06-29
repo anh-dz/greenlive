ea387c7fb8f0795863b154f6798f19a78c0126aa
Mink Alex Vina <64893437+anh-dz@users.noreply.github.com>
2023-06-29 18:27:19 +0700

    hetcuu

--- view/sell.py
+++ view/sell.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import *
 
 seller = Blueprint('seller', __name__)
 
-@seller.route('/seller')
+@seller.route('/seller', methods=['POST', 'GET'])
 def au1():
+    if request.method == 'POST':
+        if request.form.get('action1') == 'mdh':
+            tendonhang = request.form.get('tendonhang')
+            madonhang = request.form.get('madonhang')
+            nhapdiem = request.form.get('nhapdiem')
+        elif  request.form.get('action2') == 'magiamgia':
+            pass # do something else
     return render_template("seller.html")
